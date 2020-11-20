85d4c50aac562c0635c3071ce3cd8fbfffd795bb
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2020-11-20 19:28:30 -0300

    Add files via upload

---
+++ projPOO1/projPOO1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projPOO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret1;
+
+            ret1 = new Retangulo();
+
+            Console.Write("Informe a base do retângulo: ");
+            ret1.setBase(int.Parse(Console.ReadLine()));
+
+            Console.Write("Informe a altura do retângulo: ");
+            ret1.setAltura(int.Parse(Console.ReadLine()));
+
+            ret1.calcularArea();
+
+            Console.WriteLine("Um retângulo que tem base {0} e altura {1} tem área: {2} ", 
+                ret1.getBase(), ret1.getAltura(), ret1.getArea());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ projPOO1/projPOO1/Retangulo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projPOO1
+{
+    class Retangulo
+    {
+        private int b;
+        private int h;
+        private int a;
+
+        // Métodos de Interface (setters & getters)
+
+        public void setBase(int p)
+        {
+            b = p;
+        }
+
+        public void setAltura(int p)
+        {
+            h = p;
+        }
+
+        public int getBase()
+        {
+            return b;
+        }
+
+        public int getAltura()
+        {
+            return h;
+        }
+
+        public int getArea()
+        {
+            return a;
+        }
+
+        public void calcularArea()
+        {
+            a = b * h;
+        }
+    }
+}
